cf8e717cc3bd22e2617222a0b613701a31bb8271
arina-ust <arina.ust@gmail.com>
2022-09-07 22:11:24 +0200

    WIP day 16

--- day16/puzzle.go
+++ day16/puzzle.go
@@ -2,22 +2,24 @@ package day16
 
 import (
 	"advent-of-code-2021/util"
+	"encoding/hex"
 	"fmt"
-	"strings"
+	"strconv"
 )
 
-const name = "day16"
+const day = "day16"
 
 var inputFile string
 
 func Solve(easy bool) (name string, res string, err error) {
+	name = day
 	setInput(easy)
 	lines, err := util.ReadStringList(inputFile)
 
 	if easy {
-		res = partOne(lines)
+		res, err = partOne(lines)
 	} else {
-		return name, "", fmt.Errorf("not solved yet")
+		return day, "", fmt.Errorf("not solved yet")
 	}
 
 	return
@@ -25,12 +27,93 @@ func Solve(easy bool) (name string, res string, err error) {
 
 func setInput(easy bool) {
 	if easy {
-		inputFile = name + "/" + util.InputFileEasy
+		inputFile = day + "/" + util.InputFileEasy
 	} else {
-		inputFile = name + "/" + util.InputFileFull
+		inputFile = day + "/" + util.InputFileFull
 	}
 }
 
-func partOne(lines []string) string {
-	return strings.Join(lines, "\n") // TODO: solve
+func partOne(lines []string) (string, error) {
+	line := lines[0]
+
+	bytes, err := hex.DecodeString(line)
+	if err != nil {
+		return "", err
+	}
+
+	var signal string
+	for _, b := range bytes {
+		signal += fmt.Sprintf("%08b", b)
+	}
+	fmt.Println(signal)
+	fmt.Println(len(signal))
+
+	fmt.Println(parseVersion(signal))
+	typeID, _ := parseTypeID(signal)
+	fmt.Println(typeID)
+
+	if isLiteral(typeID) {
+		literal, _ := parseLiteral(signal[6:])
+		fmt.Println(literal)
+		fmt.Println(convertToInt(literal))
+	} else {
+		if isLengthOfSubpackets(rune(signal[6])) {
+			fmt.Println(getLengthOfSubpackets(signal[7:]))
+			firstLiteral, _ := parseLiteral(signal[22:])
+			fmt.Println(firstLiteral)
+			fmt.Println(convertToInt(firstLiteral))
+		}
+	}
+
+	return "", nil
+}
+
+func parseVersion(signal string) (int64, error) {
+	return strconv.ParseInt(signal[0:3], 2, 64)
+}
+
+func parseTypeID(signal string) (int64, error) {
+	return strconv.ParseInt(signal[3:6], 2, 64)
+}
+
+func parseLiteral(signalPart string) (string, error) {
+	var literal string
+	for i := 0; i < len(signalPart); i += 5 {
+		val, err := strconv.Atoi(string(signalPart[i]))
+		if err != nil {
+			return "", err
+		}
+
+		literal += signalPart[i+1 : i+5]
+
+		if val == 0 {
+			break
+		}
+
+	}
+	// TODO return also index = i  + necesarry padding accounted for
+	return literal, nil
+}
+
+func isLiteral(typeID int64) bool {
+	return typeID == 4
+}
+
+func convertToInt(val string) (int64, error) {
+	return strconv.ParseInt(val, 2, 64)
+}
+
+func isLengthOfSubpackets(lengthTypeID rune) bool {
+	if lengthTypeID == '0' {
+		return true
+	}
+	return false // ie number of subpackets
+}
+
+func getLengthOfSubpackets(signalPart string) (int64, error) {
+	return strconv.ParseInt(signalPart[0:15], 2, 64)
+}
+
+func getNumberOfSubpackets(signalPart string) (int64, error) {
+	return strconv.ParseInt(signalPart[0:11], 2, 64)
 }
