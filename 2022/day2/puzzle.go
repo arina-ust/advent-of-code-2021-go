83d5f7306aaf251b901355f3ed925330f30f7845
arina-ust <arina.ust@gmail.com>
2022-12-02 19:15:20 +0100

    Refactor Day 2

--- 2022/day2/puzzle.go
+++ 2022/day2/puzzle.go
@@ -31,120 +31,78 @@ func setInput(easy bool) {
 	}
 }
 
-const lost, draw, won int = 0, 3, 6
-const rock, paper, scissors = "rock", "paper", "scissors"
-const lose, playDraw = 'X', 'Y'
-
-type round struct {
-	name         string
-	yourResponse bool
-	score        int
-}
-
-func (r round) play(opponent round) int {
-	if r.name == opponent.name {
-		return draw
-	}
-	if r.name == rock {
-		if opponent.name == scissors {
-			return won
-		}
-		return lost
-	}
-	if r.name == paper {
-		if opponent.name == rock {
-			return won
-		}
-		return lost
-	}
-	if r.name == scissors {
-		if opponent.name == paper {
-			return won
-		}
-		return lost
-	}
-	return lost
-}
-
-func (r round) findResponseRound(result rune) round {
-	if r.name == rock {
-		if result == lose {
-			return scores['C']
-		} else if result == playDraw {
-			return scores['A']
-		} else { // win
-			return scores['B']
-		}
-	} else if r.name == paper {
-		if result == lose {
-			return scores['A']
-		} else if result == playDraw {
-			return scores['B']
-		} else { // win
-			return scores['C']
-		}
-	} else {
-		if result == lose {
-			return scores['B']
-		} else if result == playDraw {
-			return scores['C']
-		} else { // win
-			return scores['A']
-		}
-	}
-}
-
-var scores = map[rune]round{
-	'A': {
-		name:         rock,
-		yourResponse: false,
-		score:        1,
-	},
-	'B': {
-		name:         paper,
-		yourResponse: false,
-		score:        2,
-	},
-	'C': {
-		name:         scissors,
-		yourResponse: false,
-		score:        3,
-	},
-	// Not needed for part 2 -->
-	'X': {
-		name:         rock,
-		yourResponse: true,
-		score:        1,
-	},
-	'Y': {
-		name:         paper,
-		yourResponse: true,
-		score:        2,
-	},
-	'Z': {
-		name:         scissors,
-		yourResponse: true,
-		score:        3,
-	},
-}
-
 func partOne(lines []string) (string, error) {
 	total := 0
 	for _, line := range lines {
-		opponent, response := scores[rune(line[0])], scores[rune(line[2])]
-		score := response.play(opponent) + response.score
-		total += score
+		total += scores[rune(line[0])][rune(line[2])]
 	}
 	return strconv.Itoa(total), nil
+}
+
+// part 1
+//          1   2   3
+// Op/Resp  R   P   S
+//        R 1+3 2+6 3+0
+//        P 1+0 2+3 3+6
+//        S 1+6 2+0 3+3
+//
+
+var scores = map[rune]map[rune]int{
+	'A': {
+		'X': 4,
+		'Y': 8,
+		'Z': 3,
+	},
+	'B': {
+		'X': 1,
+		'Y': 5,
+		'Z': 9,
+	},
+	'C': {
+		'X': 7,
+		'Y': 2,
+		'Z': 6,
+	},
+}
+
+// part 2
+// Score  1   2   3
+//        R   P   S
+//
+//          0 3 6
+// Op/Res   X Y Z
+//        R S R P
+//        P R P S
+//        S P S R
+//
+//          0 3 6
+// Op/Res   X Y Z
+//        R 0+3 3+1 6+2
+//        P 0+1 3+2 6+3
+//        S 0+2 3+3 6+1
+
+var scores2 = map[rune]map[rune]int{
+	'A': {
+		'X': 3,
+		'Y': 4,
+		'Z': 8,
+	},
+	'B': {
+		'X': 1,
+		'Y': 5,
+		'Z': 9,
+	},
+	'C': {
+		'X': 2,
+		'Y': 6,
+		'Z': 7,
+	},
 }
 
 func partTwo(lines []string) (string, error) {
 	total := 0
 	for _, line := range lines {
-		opponent := scores[rune(line[0])]
-		response := opponent.findResponseRound(rune(line[2]))
-		score := response.play(opponent) + response.score
-		total += score
+		total += scores2[rune(line[0])][rune(line[2])]
 	}
 	return strconv.Itoa(total), nil
 }
