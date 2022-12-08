0fe1c1d6a577bb13af31addea439b67587697509
arina-ust <arina.ustyantseva@storytel.com>
2022-12-08 10:55:45 +0100

    Day 8 Part 2

--- 2022/day8/puzzle.go
+++ 2022/day8/puzzle.go
@@ -17,8 +17,8 @@ func Solve(easy bool) (name string, res string, err error) {
 		return
 	}
 
-	res, err = partOne(matrix)
-	// res, err = partTwo(lines)
+	// res, err = partOne(matrix)
+	res, err = partTwo(matrix)
 
 	return
 }
@@ -98,4 +98,66 @@ func partOne(matrix [][]int) (string, error) {
 	}
 
 	return strconv.Itoa(count), nil
+}
+
+func partTwo(matrix [][]int) (string, error) {
+	right := len(matrix[0]) - 1
+	left := 0
+	top := 0
+	bottom := len(matrix) - 1
+
+	highestScore := 1
+
+	for i := 1; i < right; i++ {
+		for j := 1; j < bottom; j++ {
+			treeHeight := matrix[i][j]
+			score := 1
+
+			// look right
+			numVisible := 0
+			for c := j + 1; c <= right; c++ {
+				numVisible++
+				if matrix[i][c] >= treeHeight {
+					break
+				}
+			}
+			score = score * numVisible
+
+			// look left
+			numVisible = 0
+			for c := j - 1; c >= left; c-- {
+				numVisible++
+				if matrix[i][c] >= treeHeight {
+					break
+				}
+			}
+			score = score * numVisible
+
+			// look up
+			numVisible = 0
+			for c := i - 1; c >= top; c-- {
+				numVisible++
+				if matrix[c][j] >= treeHeight {
+					break
+				}
+			}
+			score = score * numVisible
+
+			// look down
+			numVisible = 0
+			for c := i + 1; c <= bottom; c++ {
+				numVisible++
+				if matrix[c][j] >= treeHeight {
+					break
+				}
+			}
+			score = score * numVisible
+
+			if score > highestScore {
+				highestScore = score
+			}
+		}
+	}
+
+	return strconv.Itoa(highestScore), nil
 }
