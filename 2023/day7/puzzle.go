f1c148bb7bf3639d26e596e627df0bb49fd9cd2c
arina-ust <arina.ustyantseva@storytel.com>
2023-12-08 11:27:01 +0100

    Day 7 Part 2

--- 2023/day7/puzzle.go
+++ 2023/day7/puzzle.go
@@ -47,6 +47,18 @@ func (h *hand) getStrength() int {
 		m[card] += 1
 	}
 
+	// five of kind = 7
+	// four of kind = 6
+	// full house = 5
+	// three of kind = 4
+	// two pair = 3
+	// one pair = 2
+	// high card = 1
+
+	if m['J'] == 5 {
+		// JJJJJ
+		return 7 // five of kind
+	}
 	if m['J'] == 4 {
 		// JJJJK
 		return 7 // five of kind
@@ -62,7 +74,7 @@ func (h *hand) getStrength() int {
 	}
 	if m['J'] == 2 {
 		if len(m) == 2 {
-			//// JJKKK
+			// JJKKK
 			return 7 // five of kind
 		} else if len(m) == 3 {
 			// JJKKQ
@@ -74,11 +86,22 @@ func (h *hand) getStrength() int {
 	}
 	if m['J'] == 1 {
 		if len(m) == 2 {
-			//// JKKKK
+			// JKKKK
 			return 7 // five of kind
 		} else if len(m) == 3 {
-			// JKKKQ
-			return 6 // four of kind
+			foundThree := false
+			for _, v := range m {
+				if v == 3 {
+					foundThree = true
+				}
+			}
+			if foundThree {
+				// JKKKQ
+				return 6 // four of kind
+			} else {
+				// JKKQQ
+				return 5 // full house
+			}
 		} else if len(m) == 4 {
 			// JKKTQ
 			return 4 // three of kind
@@ -89,43 +112,32 @@ func (h *hand) getStrength() int {
 	}
 
 	if len(m) == 5 { // hand size
-//		if m['J'] != 0 {
-//			return 2 // one pair
-//		}
+		// 23456
 		return 1 // high card
 	}
 
 	for _, v := range m {
 		if v == 5 {
+			// AAAAA
 			return 7 // five of kind
 		} else if v == 4 {
-//			if m['J'] != 0 {
-//				return 7 // five of kind
-//			}
+			// AA8AA
 			return 6 // four of kind
 		} else if v == 3 {
 			if len(m) == 2 {
-//				if m['J'] != 0 {
-//					return 7 // five of kind
-//				}
+				// 23332
 				return 5 // full house
 			}
 			if len(m) == 3 {
-//				if m['J'] != 0 {
-//					return 4 + 1 + m['J']// four of kind
-//				}
+				// TTT98
 				return 4 // three of kind
 			}
 		} else if v == 2 {
 			if len(m) == 3 {
-//				if m['J'] != 0 {
-//					return 3 + 1 + m['J'] // full house OR four of a kind
-//				}
+				// 23432
 				return 3 // two pair
 			} else if len(m) == 4 {
-//				if m['J'] != 0 {
-//					return 2 + m['J'] // three of a kind OR two pair
-//				}
+				// A23A4
 				return 2 // one pair
 			}
 		}
@@ -134,7 +146,7 @@ func (h *hand) getStrength() int {
 }
 
 var cardStrengths = map[rune]int{'A': 13, 'K': 12, 'Q': 11, 'J': 0, 'T': 9, '9': 8,
-	'8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1,}
+	'8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1}
 
 func (h *hand) compareLess(other *hand) bool {
 	if h.typeStrength > other.typeStrength {
@@ -147,13 +159,13 @@ func (h *hand) compareLess(other *hand) bool {
 		rh := rune(h.cards[i])
 		ro := rune(other.cards[i])
 		if cardStrengths[rh] > cardStrengths[ro] {
-//			fmt.Printf("rh %s turned out bigger than ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
+			//			fmt.Printf("rh %s turned out bigger than ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
 			return false
 		} else if cardStrengths[rh] < cardStrengths[ro] {
-//			fmt.Printf("rh %s turned out smaller than ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
+			//			fmt.Printf("rh %s turned out smaller than ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
 			return true
 		}
-//		fmt.Printf("rh %s was the same as ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
+		//		fmt.Printf("rh %s was the same as ro %s with values %v %v\n", string(rh), string(ro), cardStrengths[rh], cardStrengths[ro])
 	}
 	fmt.Println("shouldn't be here")
 	return false
@@ -172,16 +184,16 @@ func partOne(lines []string) (int, error) {
 		hand.typeStrength = strgth
 		hands[i] = hand
 	}
-	
+
 	sort.Slice(hands, func(i, j int) bool {
 		return hands[i].compareLess(hands[j])
 	})
-	
+
 	res := 0
 	for j, h := range hands {
-		h.rank = j+1
+		h.rank = j + 1
 		res += h.rank * h.bid
 	}
-	
+
 	return res, nil
 }
--- 2023/main.go
+++ 2023/main.go
@@ -9,7 +9,7 @@ func main() {
 
 	fmt.Println("Calculating solution for...")
 
-	day, res, err := day7.Solve(false) // 248717914 too low, 249623465 ???, 249821803 too high
+	day, res, err := day7.Solve(false)
 
 	fmt.Println("---> ", day, " <---")
 	fmt.Println()
