0bfbf34e5987a53ec79bf50ad9b452a94f3f4747
arina-ust <arina.ust@gmail.com>
2023-12-09 15:39:04 +0100

    Day 9 Part 2

--- 2023/day9/puzzle.go
+++ 2023/day9/puzzle.go
@@ -18,7 +18,8 @@ func Solve(easy bool) (name string, res int, err error) {
 		return
 	}
 
-	res, err = partOne(lines)
+//	res, err = partOne(lines)
+	res, err = partTwo(lines)
 
 	return
 
@@ -87,6 +88,67 @@ func partOne(lines []string) (int, error) {
 			calculations[nextI] = calculations[nextI-1] + calculations[currI]
 		}
 		
+		res += calculations[placeholderIndices[0]]
+	}
+
+	return res, nil
+}
+
+func partTwo(lines []string) (int, error) {
+	res := 0
+
+	for _, line := range lines {
+		values := strings.Split(line, " ")
+
+		// 0 7 13 18
+		var placeholderIndices []int
+		// 0(p) 0 3 6 9 12 15 0(p) 3 3 3 3 3 0(p) 0 0 0 0 0(p)
+		var calculations []int
+
+		calculations = append(calculations, 0) // append placeholder
+		placeholderIndices = append(placeholderIndices, 0)
+
+		for _, v := range values {
+			num, _ := strconv.Atoi(v)
+			calculations = append(calculations, num)
+		}
+		calculations = append(calculations, 0) // append placeholder
+		placeholderIndices = append(placeholderIndices, len(calculations)-1)
+
+		j := 1
+		k := 1
+		for {
+			if j+1 == placeholderIndices[k] {
+				k++
+				placeholderIndices = append(placeholderIndices, len(calculations))
+				calculations = append(calculations, 0) // append placeholder
+
+				sumDiffs := 0
+				for m := placeholderIndices[len(placeholderIndices)-1] - 1; m > placeholderIndices[len(placeholderIndices)-2]; m-- {
+					sumDiffs += calculations[m]
+				}
+				if sumDiffs == 0 {
+					break
+				}
+
+				j += 2
+			}
+			if j == len(calculations) {
+				break
+			}
+
+			diff := calculations[j+1] - calculations[j]
+			calculations = append(calculations, diff)
+
+			j++
+		}
+
+		for i := len(placeholderIndices) - 2; i > 0; i-- { // 2 1
+			nextPI := i-1 // 1 0
+			currI := placeholderIndices[i] // 13 7 0
+			nextI := placeholderIndices[nextPI] // 7 0
+			calculations[nextI] = calculations[nextI+1] - calculations[currI]
+		}
 		res += calculations[placeholderIndices[0]]
 	}
 
